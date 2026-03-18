532c9aa599fd755cc82df2980383d85b8058f822
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-18 18:33:13 +0100

    fix: update deprecated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
     id("com.ncorti.ktfmt.gradle") version "0.17.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.4" apply false
     id("com.diffplug.spotless") version "6.25.0" apply false
