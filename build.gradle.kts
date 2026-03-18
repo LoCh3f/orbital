2f4284fed2cd0b5dd31bacd69c36c2df77e9e127
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-03-18 18:00:52 +0100

    fix: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
+    id("com.ncorti.ktfmt.gradle") version "0.17.0" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.4" apply false
+    id("com.diffplug.spotless") version "6.25.0" apply false
 }
 allprojects {
     group = "com.orbital"
@@ -16,4 +19,7 @@ allprojects {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+    apply(plugin = "com.ncorti.ktfmt.gradle")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "com.diffplug.spotless")
 }
