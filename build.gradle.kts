5af66e9689f4148a804ddfa6733187f0a8ff879b
Aldo Visconti <aldovisco20000@gmail.com>
2026-09-17 17:16:17 +0200

    build(gradle): build.gradle.kts: guarded the subprojects block to skip orbital-app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.ncorti.ktfmt.gradle") version "0.17.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.4" apply false
     id("com.diffplug.spotless") version "6.25.0" apply false
@@ -18,6 +18,10 @@ allprojects {
 
 
 subprojects {
+    // orbital-app is a true Kotlin Multiplatform module (kotlin("multiplatform")),
+    // which cannot coexist with kotlin("jvm") being force-applied here.
+    if (name == "orbital-app") return@subprojects
+
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
     apply(plugin = "com.ncorti.ktfmt.gradle")
